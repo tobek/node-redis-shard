da57ad7e50f8cce3dbd83b7c3f3902bcf027c622
Toby Fox <toby@digg.com>
2014-06-27 19:43:58 -0400

    Allow sharded publishing
    Consistent hashing on the channel name to determine which node will
    publish

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ module.exports = function RedisShard(options) {
     "expireat", "get", "getbit", "getrange", "getset", "hdel", "hexists", "hget", "hgetall", "hincrby",
     "hincrbyfloat", "hkeys", "hlen", "hmget", "hmset", "hset", "hsetnx", "hvals", "incr", "incrby", "incrbyfloat",
     "lindex", "linsert", "llen", "lpop", "lpush", "lpushx", "lrange", "lrem", "lset", "ltrim", "mget", "move",
-    "persist", "pexpire", "pexpireat", "psetex", "pttl", "rename", "renamenx", "restore", "rpop", "rpush", "rpushx",
+    "persist", "pexpire", "pexpireat", "psetex", "pttl", "publish", "rename", "renamenx", "restore", "rpop", "rpush", "rpushx",
     "sadd", "scard", "sdiff", "set", "setbit", "setex", "setnx", "setrange", "sinter", "sismember", "smembers",
     "sort", "spop", "srandmember", "srem", "strlen", "sunion", "ttl", "type", "watch", "zadd", "zcard", "zcount",
     "zincrby", "zrange", "zrangebyscore", "zrank", "zrem", "zremrangebyrank", "zremrangebyscore", "zrevrange",
@@ -53,7 +53,7 @@ module.exports = function RedisShard(options) {
     "auth", "bgrewriteaof", "bgsave", "bitop", "brpoplpush", "client kill", "client list", "client getname",
     "client setname", "config get", "config set", "config resetstat", "dbsize", "debug segfault", "discard",
     "echo", "eval", "evalsha", "exec", "flushall", "flushdb", "info", "keys", "lastsave", "migrate", "monitor",
-    "mset", "msetnx", "multi", "object", "ping", "psubscribe", "publish", "punsubscribe", "quit", "randomkey",
+    "mset", "msetnx", "multi", "object", "ping", "psubscribe", "punsubscribe", "quit", "randomkey",
     "rpoplpush", "save", "script exists", "script flush", "script kill", "script load", "sdiffstore", "select",
     "shutdown", "sinterstore", "slaveof", "slowlog", "smove", "subscribe", "sunionstore", "sync", "time",
     "unsubscribe", "unwatch", "zinterstore", "zunionstore"
